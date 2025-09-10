c8344b52df8f2338f2a35299cb55323c3b8e589d
leon-rgb <56979997+leon-rgb@users.noreply.github.com>
2025-09-10 21:14:34 +0200

    removed unneccessary script and tiny changes to corsConfig

--- miniconcurexpense/check1.ps1
+++
@@ -1,2 +0,0 @@
-$pod = kubectl -n miniconcur get pod -l app=frontend -o jsonpath='{.items[0].metadata.name}'
-kubectl -n miniconcur exec -it $pod -- grep -n 'localhost:30080' /usr/share/nginx/html/assets/index-*.js
--- miniconcurexpense/src/main/java/io/github/leon_rgb/miniconcurexpense/CorsConfig.java
+++ miniconcurexpense/src/main/java/io/github/leon_rgb/miniconcurexpense/CorsConfig.java
@@ -1,11 +1,17 @@
 package io.github.leon_rgb.miniconcurexpense;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+/***
+ * Configuration is only needed for local development with frontend running on a different port.
+ * In production, the frontend would be served by the backend, so CORS is not an issue
+ */
 @Configuration
+@Profile("dev")
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
